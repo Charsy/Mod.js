65a9abd4a4edd5b82d0f8385668bdb513445f9b1
Charsy <33355898+Charsy@users.noreply.github.com>
2018-01-28 15:26:49 +0600

    Mod.js and Mod.css
    Main script file of library and main stylefile.

---
+++ mod.js
@@ -0,0 +1,22 @@
+//Mod.js ver:0.1
+var modvk = "http://vk.com/modjs"
+
+$(function(){
+	
+	$(".hide_from").click(function(){
+		
+		$(".hide_to").hide();
+		
+	});
+	
+});
+
+$(function(){
+	
+	$(".fadeout_from").click(function(){
+		
+		$(".fadeout_to").fadeOut(1000);
+		
+	});
+});
+
